70567b65ccd9e638a463f38487650a6906713a65
tuanduy010806 <24020109@vnu.edu.vn>
2025-10-31 21:04:44 +0700

    sua doi file main

--- src.main/main.cpp
+++ src.main/main.cpp
@@ -1,15 +1,22 @@
 #include "core/Game.h"
 #include "states/MenuState.h"
 #include <iostream>
+#include <windows.h>   // 🔹 thêm dòng này
 
 int main(int argc, char* argv[]) {
+    // 🔹 Ẩn cửa sổ console (nếu có)
+    HWND hwnd = GetConsoleWindow();
+    ShowWindow(hwnd, SW_HIDE);
+
     Game* game = Game::Instance();
     if (!game->Init("BricksBreaker", 800, 600)) {
         std::cerr << "Game initialization failed!" << std::endl;
         return -1;
     }
+
     game->ChangeState(new MenuState());
     game->Run();
     game->Clean();
+
     return 0;
 }
