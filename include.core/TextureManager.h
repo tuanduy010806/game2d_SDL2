e9bf56d47945dc0fa662370b351795932ac99625
tuanduy010806 <24020109@vnu.edu.vn>
2025-11-02 20:25:42 +0700

    bo drawframe()

--- include.core/TextureManager.h
+++ include.core/TextureManager.h
@@ -7,17 +7,17 @@
 class TextureManager {
 public:
     static TextureManager* Instance();
-
     bool Load(const std::string& id, const std::string& file, SDL_Renderer* renderer);
     void Draw(const std::string& id, int x, int y, int w, int h, SDL_Renderer* renderer);
-    void DrawFrame(const std::string& id, int x, int y, int w, int h,
-                   int row, int frame, SDL_Renderer* renderer, double angle = 0.0);
     void ClearFromTextureMap(const std::string& id);
     void Clean();
-
 private:
     TextureManager() {}
+    ~TextureManager() {}
+    TextureManager(const TextureManager&) = delete;
+    TextureManager& operator=(const TextureManager&) = delete;
     std::map<std::string, SDL_Texture*> m_textureMap;
     static TextureManager* s_instance;
 };
-#endif
+
+#endif // TEXTUREMANAGER_H
