8544ba5097c599e910e4ab85ecefc15606765247
tuanduy010806 <24020109@vnu.edu.vn>
2025-11-02 20:36:25 +0700

    bo getstateid()

--- include.states/GameState.h
+++ include.states/GameState.h
@@ -1,17 +1,17 @@
 #ifndef GAMESTATE_H
 #define GAMESTATE_H
-#include <string>
 #include <SDL.h>
+#include <string>
 
 class GameState {
 public:
+    //  destructor
     virtual ~GameState() {}
-
     virtual void Enter() = 0;
     virtual void HandleEvents(SDL_Event& e) = 0;
     virtual void Update() = 0;
     virtual void Render() = 0;
     virtual void Exit() = 0;
-    virtual std::string GetStateID() const = 0;
 };
-#endif
+
+#endif // GAMESTATE_H
