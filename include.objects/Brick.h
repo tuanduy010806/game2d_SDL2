70594aa296c7b4abf73546b825e2ddacb7fde243
tuanduy010806 <24020109@vnu.edu.vn>
2025-11-02 20:32:32 +0700

    them bien kiem soat brick hp=2

--- include.objects/Brick.h
+++ include.objects/Brick.h
@@ -2,19 +2,21 @@
 #define BRICK_H
 #include <SDL.h>
 #include <string>
-#include "core/TextureManager.h"
 
 class Brick {
 public:
     Brick(int x, int y, int w, int h, int hp, const std::string& textureID);
-
     void Draw(SDL_Renderer* renderer);
     bool IsAlive() const { return m_hp > 0; }
     void Hit();
     SDL_Rect GetRect() const { return m_rect; }
+    int GetHp() const { return m_hp; }
+    int GetOriginalHp() const { return m_originalHp; }
 private:
     SDL_Rect m_rect;
     int m_hp;
+    int m_originalHp;
     std::string m_textureID;
 };
-#endif
+
+#endif // BRICK_H
