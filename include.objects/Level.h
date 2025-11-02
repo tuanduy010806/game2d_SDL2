d1cfabb973f5fcc0f9cf141f0539a6b617690b16
tuanduy010806 <24020109@vnu.edu.vn>
2025-11-02 15:54:32 +0700

    healder cua level

---
+++ include.objects/Level.h
@@ -0,0 +1,19 @@
+#ifndef LEVEL_H
+#define LEVEL_H
+#include <vector>
+#include "objects/Brick.h"
+#include <SDL.h>
+
+class Level {
+public:
+    Level();
+    void LoadMap(int mapIndex, SDL_Renderer* renderer);
+    void Draw(SDL_Renderer* renderer);
+    std::vector<Brick>& GetBricks() { return m_bricks; }
+    bool AllCleared() const;
+
+private:
+    std::vector<Brick> m_bricks;
+    int m_tileW, m_tileH;
+};
+#endif
