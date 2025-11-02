3af7feb8dafe0022545e95fa10353c785027de26
tuanduy010806 <24020109@vnu.edu.vn>
2025-11-02 20:38:33 +0700

    them panel_react

--- include.states/LoseState.h
+++ include.states/LoseState.h
@@ -1,20 +1,23 @@
 #ifndef LOSESTATE_H
 #define LOSESTATE_H
-#include "states/GameState.h"
-#include <SDL.h>
+#include "GameState.h"
+#include <SDL_rect.h>
 
+union SDL_Event;
 class LoseState : public GameState {
 public:
     LoseState(float record);
+    virtual ~LoseState();
     void Enter() override;
     void HandleEvents(SDL_Event& e) override;
     void Update() override;
     void Render() override;
     void Exit() override;
-    std::string GetStateID() const override { return "LOSE"; }
 
 private:
     float m_record;
     SDL_Rect m_restartButton;
+    SDL_Rect m_panelRect;
 };
-#endif
+
+#endif // LOSESTATE_H
