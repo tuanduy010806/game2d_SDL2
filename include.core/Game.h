cf332dc1feae92411279d35255d937ef051f1512
tuanduy010806 <24020109@vnu.edu.vn>
2025-11-02 20:24:14 +0700

    them ham run()

--- include.core/Game.h
+++ include.core/Game.h
@@ -1,6 +1,6 @@
 #ifndef GAME_H
 #define GAME_H
-#include <SDL2/SDL.h>
+#include <SDL.h>
 #include <vector>
 #include <string>
 
@@ -9,22 +9,17 @@ class GameState;
 class Game {
 public:
     static Game* Instance();
-
     bool Init(const std::string& title, int width, int height);
+    void Run();
     void HandleEvents();
     void Update();
     void Render();
     void Clean();
-
     bool Running() const { return m_running; }
     void Quit() { m_running = false; }
-
     SDL_Renderer* GetRenderer() const { return m_renderer; }
     int GetWidth() const { return m_width; }
     int GetHeight() const { return m_height; }
-
-    const std::vector<GameState*>& GetStates() const { return m_states; }
-
     void PushState(GameState* state);
     void PopState();
     void ChangeState(GameState* state);
@@ -33,14 +28,14 @@ public:
 private:
     Game();
     ~Game();
-
+    Game(const Game&) = delete;
+    Game& operator=(const Game&) = delete;
     static Game* s_instance;
-
     SDL_Window* m_window;
     SDL_Renderer* m_renderer;
     bool m_running;
     int m_width, m_height;
-
     std::vector<GameState*> m_states;
 };
-#endif
+
+#endif // GAME_H
