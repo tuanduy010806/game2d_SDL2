75d1efd6f7c72ba345093e5035a24345cc6c0d93
tuanduy010806 <24020109@vnu.edu.vn>
2025-11-02 20:22:17 +0700

    them ham tao hieu ung led

--- include.core/FontManager.h
+++ include.core/FontManager.h
@@ -1,5 +1,6 @@
 #ifndef FONTMANAGER_H
 #define FONTMANAGER_H
+
 #include <SDL_ttf.h>
 #include <string>
 #include <map>
@@ -9,14 +10,26 @@ public:
     static FontManager* Instance();
 
     bool Load(const std::string& id, const std::string& path, int size);
-    // Đã thêm SDL_Renderer*
+
     void DrawText(const std::string& id, const std::string& text,
                   SDL_Color color, int x, int y, SDL_Renderer* renderer);
+
+    //  hiệu ứng phát sáng LED
+    void DrawTextWithGlow(const std::string& id, const std::string& text,
+                          SDL_Color mainColor, SDL_Color glowColor,
+                          int x, int y, SDL_Renderer* renderer, int intensity = 6);
+
+    void ClearFromTextureMap(const std::string& id);
     void Clean();
 
 private:
     FontManager() {}
+    ~FontManager() {}
+    FontManager(const FontManager&) = delete;
+    FontManager& operator=(const FontManager&) = delete;
+
     static FontManager* s_instance;
     std::map<std::string, TTF_Font*> m_fontMap;
 };
-#endif
+
+#endif // FONTMANAGER_H
