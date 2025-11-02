749f6f8014f6f4e875fa8297a6c6c1e36a7964a3
tuanduy010806 <24020109@vnu.edu.vn>
2025-11-02 20:33:36 +0700

    them comment

--- include.objects/Level.h
+++ include.objects/Level.h
@@ -9,11 +9,14 @@ public:
     Level();
     void LoadMap(int mapIndex, SDL_Renderer* renderer);
     void Draw(SDL_Renderer* renderer);
+    //check danh sách gạch xem bóng có đụng trúng viên nào không
     std::vector<Brick>& GetBricks() { return m_bricks; }
+    // check viên nào trong map còn sống
     bool AllCleared() const;
 
 private:
     std::vector<Brick> m_bricks;
     int m_tileW, m_tileH;
 };
-#endif
+
+#endif // LEVEL_H
