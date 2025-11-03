d0be133e6e0bb1fd68339ec03643f724281d26fd
tuanduy010806 <24020109@vnu.edu.vn>
2025-11-03 15:01:53 +0700

    hoan thien paddle

--- src.objects/Paddle.cpp
+++ src.objects/Paddle.cpp
@@ -1,12 +1,12 @@
 #include "objects/Paddle.h"
 #include "core/TextureManager.h"
 #include "core/Utils.h"
-// khởi tạo
+// khởi tạo paddle
 Paddle::Paddle(int x, int y, int w, int h, const std::string& textureID)
     : m_textureID(textureID) {
     m_rect = {x, y, w, h};
 }
-// update vtri theo chuột
+// update vtri paddle theo chuột
 void Paddle::Update(int mouseX, int windowWidth) {
     m_rect.x = (int)Utils::Clamp((float)mouseX - m_rect.w / 2.0f, 0.0f, (float)(windowWidth - m_rect.w));
 }
